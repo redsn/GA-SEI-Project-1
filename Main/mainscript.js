0912c71ec95a3d046ef78fcd86fd55de2bc779f6
tkl <tluk92@gmail.com>
2022-06-09 01:07:34 -0400

    HTML & CSS basic framework ALPHA V2

--- Main/mainscript.js
+++ Main/mainscript.js
@@ -1,3 +1,22 @@
+let naturesObject = {
+    //all pokemon natures declared
+}
+
+new class theQuiz {
+    //Class object for questions
+    //Will modify naturesObject array
+    //Add to each nature based on answer
+    //Quiz will call resolution
+}
+
+let resolution = () => {
+    // checks naturesObject for highest nature
+    // display nature
+    // display generic text for story
+}
+
+
+// ///////////////////////////// Functional fetch URL # disabled by courtesy
 let url = 'https://pokeapi.co/api/v2/generation/3/';
 let url2 = 'https://pokeapi.co/api/v2/pokemon/1/'
 let array1 = [];
@@ -7,71 +26,18 @@ fetch(url)
         return result.json()
     })
     .then(result =>{
-        // console.log(result);
+        console.log(result);
         result.pokemon_species.forEach(element => {
                 array1.push(element)})
     })
 
-fetch(url2)
-    .then(result =>{
-        return result.json()
-    })
-    .then(result =>{
-        console.log(result);
-    })
+// fetch(url2)
+//     .then(result =>{
+//         return result.json()
+//     })
+//     .then(result =>{
+//         console.log(result);
+//     })
 
-console.log(array1);
-
-
-// var data = [
-//     {
-//         "id": 30000,
-//         "display_name": "John Doe",
-//         "email": "Johndoes@testmail.com"
-//     },
-//     {
-//         "id": 30001,
-//         "display_name": "John Cena",
-//         "email": "JohnCens@testmail.com"
-//     },
-//     {
-//         "id": 30002,
-//         "display_name": "John kabaraya",
-//         "email": "Johnkabarayas@testmail.com"
-//     }
-// ]
-
-// var ids = [];
-// var display_names = [];
-// var emails = [];
-
-// function result(data) {
-
-//     data.forEach(element => {
-//         ids.push(element.id);
-//         display_names.push(element.display_name);
-//         emails.push(element.email);
-//     });
-
-//     return { "ids": ids, "display-names": display_names, "emails": emails };
-// }
-
-// console.log(result(data));
-
-// fetch(url)
-//   .then(res => {
-//     //   if (res.ok){
-//     //       console.log('success')
-//     //       return res.json
-//     //   }
-//     //   return 'aaaaaaaa'
-//     // console.log("success!");
-//     return res.json();
-//   })
-//   .then(res => {
-//       console.log(res)
-//   })
-//   .catch(err => {
-//     console.log("something went wrong...", err);
-//   })
-//   .finally(() => console.log('Request was made'))
+// console.log(array1);
+// ///////////////////////////////////
